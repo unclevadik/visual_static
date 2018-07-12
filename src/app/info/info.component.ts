d1fd20974cb245926395f63b671584f4211e9517
unclevadik <32315590+unclevadik@users.noreply.github.com>
2018-07-12 10:49:35 -0400

    Add files via upload

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
-  styleUrls: ['./info.component.css']
 })
 export class InfoComponent implements OnInit {
 
--- src/app/visual/visual.component.ts
+++ src/app/visual/visual.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-visual',
   templateUrl: './visual.component.html',
-  styleUrls: ['./visual.component.css']
 })
 export class VisualComponent implements OnInit {
 
